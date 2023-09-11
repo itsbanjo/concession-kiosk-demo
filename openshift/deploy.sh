a6968c962fb0edb8f35c84771ed1f118f721ba6a
Banjo Donila <mariel.donila@gmail.com>
2023-09-11 22:00:35 +1200

    updated

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 oc new-project concession
-oc new-app https://github.com/jankleinert/concession-kiosk-backend --name backend -e MONGODB_USER=concession -e MONGODB_PASSWORD=hello1234 -e DATABASE_SERVICE_NAME=mongodb -e MONGODB_DATABASE=concession
-oc new-app https://github.com/jankleinert/concession-kiosk-frontend --name frontend -e COMPONENT_BACKEND_HOST=backend -e COMPONENT_BACKEND_PORT=8080
+oc new-app https://github.com/itsbanjo/concession-kiosk-backend --name backend -e MONGODB_USER=concession -e MONGODB_PASSWORD=hello1234 -e DATABASE_SERVICE_NAME=mongodb -e MONGODB_DATABASE=concession
+oc new-app https://github.com/itsbanjo/concession-kiosk-frontend --name frontend -e COMPONENT_BACKEND_HOST=backend -e COMPONENT_BACKEND_PORT=8080
 oc import-image registry.access.redhat.com/rhscl/mongodb-36-rhel7 --confirm
 oc process -f template.yaml -p MONGODB_USER=concession -p MONGODB_DATABASE=concession -p MONGODB_PASSWORD=hello1234 -p NAMESPACE=concession | oc apply -f -
 oc expose svc frontend
